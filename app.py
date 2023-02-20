f25a223fd8a1a430341e6ae42303e3f98e1597c6
johncjohn <95538052+johncjohn@users.noreply.github.com>
2023-02-20 08:00:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ openai.api_key = os.getenv("OPENAI_API_KEY")## Call the API key under your accou
                                             ##and store it in .env file
 #db_url = os.environ.get('DATABASE_URL')
 #conn = psycopg2.connect(db_url)
-engine = create_engine(os.getenv('DATABASE_URL'))
+#engine = create_engine(os.getenv('DATABASE_URL'))
 engine = create_engine("postgres://admin:LK1joKixSkHrItiDOyhAneLKIrWwmsv9@dpg-cfp0vk82i3mo4bvetdjg-a.oregon-postgres.render.com/institute")
 session = Session(bind=engine)
 from app import app, db
