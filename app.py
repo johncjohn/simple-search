9fdc4447747c87c40712f59bb860e8093b08969b
huancyhuan <youhancy@gmail.com>
2023-02-25 06:26:58 +0530

    okay

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ def login_required(role="ANY"):
  
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('login.html')
 @app.route('/')
 @login_required()
 def home():
