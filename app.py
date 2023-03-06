36d2f4a959d8311e6acebfcb063b6290b240fd23
John <johncyriljohn@gmail.com>
2023-03-06 09:07:43 +0530

    okay

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ app = Flask(__name__)
 app.secret_key = "my_secret_key"
 openai.api_key = os.getenv("OPENAI_API_KEY")## Call the API key under your account (in a secure way) 
                                             ##and store it in .env file
-engine = create_engine(os.getenv('DATABASE_URL'))
-#engine = create_engine("postgresql://admin:LK1joKixSkHrItiDOyhAneLKIrWwmsv9@dpg-cfp0vk82i3mo4bvetdjg-a.oregon-postgres.render.com/institute")
+#engine = create_engine(os.getenv('DATABASE_URL'))
+engine = create_engine("postgresql://admin:LK1joKixSkHrItiDOyhAneLKIrWwmsv9@dpg-cfp0vk82i3mo4bvetdjg-a.oregon-postgres.render.com/institute")
 Base.metadata.create_all(bind=engine)
 Session = sessionmaker(bind=engine, query_cls=CustomQuery)
 dbsession = Session()
